796694f6ad935450ffcf9ee4717a8067452228ec
ZhatkinVyacheslav <90867068+ZhatkinVyacheslav@users.noreply.github.com>
2023-03-15 21:35:57 +0300

    вроде готовый класс линейной таблицы на массиве

--- lib_LinTabMass/lib_LinTabMass.h
+++ lib_LinTabMass/lib_LinTabMass.h
@@ -16,15 +16,47 @@ public:
 	}
 
 	void add(std::string name1, polinom pol) {
+		if (size == maxsize) massFix();
 		names[size] = name1;
 		polinoms[size] = pol;
+		size++;
 	}
 	
-	void deletePol(int pos);
+	void deletePol(int pos) {
+		if (size == 0) throw std::logic_error("beda");
+		else
+		{
+			for (int i = pos; i < size; i++) {
+				names[i] = names[i + 1];
+				polinoms[i] = polinoms[i + 1];
+			}
+			size--;
+		}
+	}
 
-	polinom find(std::string findName);
+	polinom find(std::string findName) {
+		for (int i = 0; i < size; i++) {
+			if (findName == names[i]) return polinoms[i];
+		}
+		polinom null;
+		return null;
+	}
 
-
+	void massFix() {
+		std::string* namesCopy = new std::string[maxsize];
+		polinom* polinomsCopy = new polinom[maxsize];
+		maxsize += 3;
+		for (int i = 0; i < size; i++) {
+			namesCopy[i] = names[i];
+			polinomsCopy[i] = polinoms[i];
+		}
+		names = new std::string[maxsize];
+		polinoms = new polinom[maxsize];
+		for (int i = 0; i < size; i++) {
+			names[i] = namesCopy[i];
+			polinoms[i] = polinomsCopy[i];
+		}
+	}
 
 	~LinTabMass() {
 		delete[] names;
