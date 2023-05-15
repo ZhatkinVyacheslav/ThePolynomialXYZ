fa296a314ee5e2fd67b91fb8f6aa213db3c87561
ZhatkinVyacheslav <90867068+ZhatkinVyacheslav@users.noreply.github.com>
2023-05-16 00:42:52 +0300

    не рабочая по непонятным причинам таблица на листе

--- lib_LinTabList/lib_LinTabList.h
+++ lib_LinTabList/lib_LinTabList.h
@@ -3,18 +3,86 @@
 #include "../lib_list/list.h"
 #include "../lib_TableInterface/lib_TableInterface.h"
 
-class LinTableList : public Table
+class DataLinTableList
 {
-	Clist <std::string> names;
-	Clist <polinom> polinoms;
+private:
+	std::string name;
+	polinom pol;
 
 public:
-	LinTableList() {}
+	DataLinTableList(std::string _name, polinom _pol) {
+		name = _name;
+		pol = _pol;
+	}
 
-	void add(std::string name1, polinom pol) override;
-	void destroyPol(int pos)override;
-	polinom find(std::string findName) override;
-	void findAndReplace(std::string findName, polinom pol) override;
+	DataLinTableList& operator= (const DataLinTableList& right) {
+		this->name = right.name;
+		this->pol = right.pol;
+	}
+
+	std::string getName() { return name; }
+	polinom getPolinom() { return pol; }
+	void setPolinom(polinom newPol) { pol = newPol; }
+
+	friend class LinTableList;
+};
+
+class LinTableList : public Table
+{
+	Clist <DataLinTableList> LData;
+public:
+	LinTableList() {
+		polinom _pol;
+		add("", _pol);
+	}
+
+	LinTableList(std::string _name, polinom _pol)
+	{
+		add(_name, _pol);
+	}
+
+	void add(std::string _name, polinom _pol) override
+	{
+		DataLinTableList d1(_name, _pol);
+		LData.push_back(d1);
+	}
+
+	void destroyPol(std::string DestroyName)override
+	{
+		for (int i = 0; i < LData.size(); i++)
+		{
+			if (LData.GetIndEl(i).name == DestroyName)
+			{
+				LData.erasePos(i);
+			}
+		}
+	}
+	polinom find(std::string findName) override
+	{
+		for (int i = 0; i < LData.size(); i++)
+		{
+			if (LData.GetIndEl(i).name == findName)
+			{
+				return LData.GetIndEl(i).pol;
+			}
+		};
+		throw std::logic_error("Error! Net polinoma s takim imenem");
+	}
+
+	void findAndReplace(std::string findName, polinom _pol) override
+	{
+		bool flag = true;
+		for (int i = 0; i < LData.size(); i++)
+		{
+			if (LData.GetIndEl(i).name == findName)
+			{
+				LData.GetIndEl(i).pol = _pol;
+				flag = false;
+			}
+		};
+
+		if (flag) throw std::logic_error("Error! Net polinoma s takim imenem");
+	}
 
 	~LinTableList() {
 	}
@@ -22,4 +90,5 @@ public:
 private:
 
 };
+
 
