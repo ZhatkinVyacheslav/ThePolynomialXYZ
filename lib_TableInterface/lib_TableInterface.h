a93bf5a0a0d579b06ffcfeccdc6526a81fb19ae0
ZhatkinVyacheslav <90867068+ZhatkinVyacheslav@users.noreply.github.com>
2023-05-16 00:40:21 +0300

    чуть чуть изменённый интерфейс

--- lib_TableInterface/lib_TableInterface.h
+++ lib_TableInterface/lib_TableInterface.h
@@ -1,11 +1,12 @@
+#pragma once
 #include <iostream>
 #include "../lib_polinom/lib_polinom.h"
 
 class Table
 {
 public:
-	void virtual add(std::string name1, polinom pol) = 0;
-	void virtual destroyPol(int pos) = 0;
-	polinom virtual find(std::string findName) = 0;
-	void virtual findAndReplace(std::string findName, polinom pol) = 0;
+	 virtual void add(std::string name1, polinom pol) = 0;
+	virtual void destroyPol(std::string DestroyName) = 0;
+	virtual polinom find(std::string findName) = 0;
+	virtual void findAndReplace(std::string findName, polinom pol) = 0;
 };
